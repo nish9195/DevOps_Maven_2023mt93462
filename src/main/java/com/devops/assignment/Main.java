f64d090cd9bbf13e311e182edc65dad434199791
Nishanth T <t.nishanth98@gmail.com>
2024-05-11 11:09:25 +0530

    Jenkins Trigger

--- src/main/java/com/devops/assignment/Main.java
+++ src/main/java/com/devops/assignment/Main.java
@@ -3,5 +3,6 @@ package com.devops.assignment;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Pushing some changes for automatic Jenkins trigger");
     }
 }
